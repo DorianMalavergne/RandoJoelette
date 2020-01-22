66556eb1acb0fa1c6c4c183b14a3cca04341c9dd
Dorian <dorianmalavergne@gmail.com>
2020-01-22 23:49:59 +0100

    add : première requête HTTP avec l'application android (backend local)

--- app/src/main/java/com/example/randojoelette/MainActivity.java
+++ app/src/main/java/com/example/randojoelette/MainActivity.java
@@ -2,7 +2,18 @@ package com.example.randojoelette;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView requete = (TextView) findViewById(R.id.requete);
+
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url = "http://192.168.43.92:8080/bonjour";
+
+        Button btn_connexion = (Button) findViewById(R.id.btn_connexion);
+        btn_connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                requete.setText(response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        requete.setText(error.toString());
+                    }
+                });
+
+                queue.add(stringRequest);
+            }
+        });
     }
 }
