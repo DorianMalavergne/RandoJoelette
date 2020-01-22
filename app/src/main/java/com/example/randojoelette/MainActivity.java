d7bf0ee041417163218b5865cf712d052648ffda
Dorian Malavergne <dorianmalavergne@gmail.com>
2020-01-22 18:00:00 +0100

    fix : modification du lancement de la vue principale

--- app/src/main/java/com/example/randojoelette/MainActivity.java
+++ app/src/main/java/com/example/randojoelette/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.asso_main_activity);
+        setContentView(R.layout.activity_main);
     }
 }
