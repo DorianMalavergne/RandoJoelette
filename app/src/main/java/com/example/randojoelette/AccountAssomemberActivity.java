56e994780ae49c880c8bb8bd974dee95f13ec1ff
Dorian Malavergne <dorianmalavergne@gmail.com>
2020-01-24 12:23:09 +0100

    add : fragment de test inscription + evenement selection spinner

--- app/src/main/java/com/example/randojoelette/AccountAssomemberActivity.java
+++ app/src/main/java/com/example/randojoelette/AccountAssomemberActivity.java
@@ -1,6 +1,10 @@
 package com.example.randojoelette;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,19 @@ public class AccountAssomemberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_assomember_activity);
+
+        Spinner spinnerStatut = (Spinner) findViewById(R.id.spinner_statut);
+
+        spinnerStatut.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Sélection", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
