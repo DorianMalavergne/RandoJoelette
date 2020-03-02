5a889383691d41ee43232f58c2ca75840263d5e9
Dorian <dorianmalavergne@gmail.com>
2020-03-02 22:30:09 +0100

    fix : modification nom background + acces assomember read active event

--- app/src/main/java/com/example/randojoelette/AssomemberMainActivity.java
+++ app/src/main/java/com/example/randojoelette/AssomemberMainActivity.java
@@ -2,6 +2,8 @@ package com.example.randojoelette;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -101,6 +103,14 @@ public class AssomemberMainActivity extends AppCompatActivity {
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listeRandonneesDisponibles);
 
         randoDispo.setAdapter(arrayAdapter);
+
+        randoDispo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(AssomemberMainActivity.this, assomember_read_active_event_activity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void afficherListeMesRando() {
--- app/src/main/java/com/example/randojoelette/assomember_read_active_event_activity.java
+++ app/src/main/java/com/example/randojoelette/assomember_read_active_event_activity.java
@@ -1,4 +1,16 @@
 package com.example.randojoelette;
 
-public class assomember_read_active_event_activity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class assomember_read_active_event_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.assomember_read_active_event_activity);
+    }
 }
